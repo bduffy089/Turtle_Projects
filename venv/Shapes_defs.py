0465315e6e42af74e3c2e781258ba4e37540bd8a
bduffy089 <bduffy898@gmail.com>
2023-07-06 19:09:04 -0700

    adding functions file

--- venv/Shapes_defs.py
+++ venv/Shapes_defs.py
@@ -29,14 +29,12 @@ def polygon(num, size):
         turtle.forward(size)
         turtle.left(360 / num)
 
-
 def spiral(len, angle):
     for i in range(len, 5, -5):
         turtle.forward(i)
         turtle.right(angle)
 
 # move functions
-
 def back(len):
     turtle.penup()
     turtle.backward(len)
