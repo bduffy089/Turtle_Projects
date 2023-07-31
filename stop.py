0a6c9db5e32c11f990316993688e764e667cedff
bduffy089 <53282905+bduffy089@users.noreply.github.com>
2023-07-30 17:35:03 -0700

    Update stop.py

--- stop.py
+++ stop.py
@@ -7,25 +7,28 @@ turtle.shape("square")  # optional
 turtle.speed(10)  # optional
 turtle.pensize(5)
 
+def stop(size):
+    octagon(size)
+    forward(3/8 * size)
+    rectangle(1/5 * size, 2 * size)
+
 def forward(len):
     turtle.penup()
     turtle.forward(len)
     turtle.pendown()
+    
 def rectangle(width, height):
     for i in range(2):
         turtle.forward(width)
         turtle.right(90)
         turtle.forward(height)
         turtle.right(90)
+        
 def octagon(len):
     for i in range(8):
         turtle.forward(len)
         turtle.left(45)
 
-def stop(size):
-    octagon(size)
-    forward(3/8 * size)
-    rectangle(1/5 * size, 2 * size)
 
 # draw stop 1
 turtle.color("MediumPurple1")
